5e3d5024f3e4f0722d707498eb409247f8689e2c
unknown <johntendik@hotmail.com>
2019-05-13 21:16:22 -0400

    broken state

--- build/jt-simple-modal.js
+++ build/jt-simple-modal.js
@@ -5,25 +5,132 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var JT_Smodals = function () {
-  function JT_Smodals(args) {
+  function JT_Smodals(options) {
+    var _this = this;
+
     _classCallCheck(this, JT_Smodals);
 
-    this.options = Object.assign(this, {
+    this.options = {
       body: "",
-      type: "",
+      type: "simple",
       header: "Attention!",
       bodyIcon: "",
-      inputType: "text",
-      headerIcon: "",
+      headerIcon: "fa-info",
       customClass: '',
       customContent: ""
-    }, args);
+    };
+
+    this.setOptions(options);
+
+    this.eventHandler = function (evt) {
+      if (evt.target && (evt.target.id == _this.options.htmlID || evt.target.id === 'jt-simple-modal-close')) {
+        _this.closeModal();
+      }
+
+      if (evt.target && evt.target.id == 'jt-simple-modal-submit') {
+        if (typeof _this.options.unlockOnClick === 'function') {
+          var val = document.getElementById('jt-simple-modal-input').value;
+          _this.options.unlockOnClick(evt, val);
+        }
+        _this.closeModal();
+      }
+
+      if (evt.target && evt.target.id == 'jt-simple-modal-confirm') {
+        if (typeof _this.options.confirmOnClick === 'function') {
+          _this.options.confirmOnClick(evt);
+        }
+        _this.closeModal();
+      }
+    };
   }
 
   _createClass(JT_Smodals, [{
-    key: "logOptions",
-    value: function logOptions() {
+    key: "setOptions",
+    value: function setOptions(options) {
+      var htmlID = 'jt-simple-modal-' + this.getNextId();
+      Object.assign(this.options, { htmlID: htmlID }, options);
       console.log(this.options);
+    }
+  }, {
+    key: "getOptions",
+    value: function getOptions() {
+      return this.options;
+    }
+  }, {
+    key: "getNextId",
+    value: function getNextId() {
+      return document.querySelectorAll('.jt-simple-modal').length;
+    }
+  }, {
+    key: "attachEvents",
+    value: function attachEvents() {
+      document.body.addEventListener('click', this.eventHandler);
+    }
+  }, {
+    key: "removeEvents",
+    value: function removeEvents() {
+      document.body.removeEventListener('click', this.eventHandler);
+    }
+  }, {
+    key: "openModal",
+    value: function openModal() {
+      var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+      // Update the options if needed
+      this.setOptions(options);
+      // Append the modal to the body
+      document.body.insertAdjacentHTML('beforeend', this.prepareModal(options));
+      // Get and Set the modal element in var
+      this.modal = document.getElementById(this.options.htmlID);
+      // Change the modal style to flex to display the modal
+      this.modal.style.display = 'flex';
+      // Attach event handler
+      this.attachEvents();
+    }
+  }, {
+    key: "closeModal",
+    value: function closeModal() {
+      // Remove the moddal from the DOM
+      this.modal.parentNode.removeChild(this.modal);
+      // Reset the modal element in var
+      this.modal = null;
+      // Remove event handler if there are no modals
+      if (this.getNextId < 1) {
+        this.removeEvents();
+      }
+    }
+  }, {
+    key: "prepareModal",
+    value: function prepareModal() {
+      var template = this.getTemplate(this.options);
+      return "\n      <div id='" + this.options.htmlID + "' class='jt-simple-modal " + this.options.customClass + "'>\n        <div id='jt-simple-modal-container'>\n          <div id='jt-simple-modal-header'>\n            <i class='fa " + this.options.headerIcon + "' aria-hidden='true'></i>\n            <h4>" + this.options.header + "</h4>\n            <i class='fa fa-times' id='jt-simple-modal-close' aria-hidden='true'></i>\n          </div>\n          <div id='jt-simple-modal-body'>\n            " + template + "\n          </div>\n        </div>\n      </div>\n    ";
+    }
+  }, {
+    key: "getTemplate",
+    value: function getTemplate(options) {
+
+      if (!options || !options.type) {
+        return;
+      }
+
+      switch (options.type) {
+        case 'simple':
+          return "\n          <div class='jt-simple-modal-desc'>\n            <i class='fa " + options.bodyIcon + "' aria-hidden='true'></i>\n            <p>" + options.body + "</p>\n          </div>\n        ";
+          break;
+
+        case 'unlock':
+          var unlockTemplate = "\n          <div class='jt-simple-modal-desc'>\n            <form id='jt-simple-modal-form'>\n              <input id=\"jt-simple-modal-input\" placeholder=\"" + (this.options.unlockPlaceholder || 'test') + "\" name=\"unlockPassword\" type=\"password\">\n            </form>\n            <a href=\"#\" class=\"btn btn-primary\" id=\"jt-simple-modal-submit\">Unlock</a>\n          </div>\n        ";
+          return unlockTemplate;
+          break;
+
+        case 'confirm':
+          return "\n          <div class='jt-simple-modal-desc'>\n            <i class='fa " + this.options.bodyIcon + "' aria-hidden='true'></i>\n            <p>" + this.options.body + "</p>\n          </div>\n          <div class='jt-simple-modal-desc'>\n            <button href=\"#\" id=\"jt-simple-modal-confirm\">" + (this.options.confirmBtnText || 'OK') + "</button>\n          </div>\n        ";
+          break;
+
+        default:
+          return "\n          <div class='jt-simple-modal-desc'>\n            <i class='fa " + options.bodyIcon + "' aria-hidden='true'></i>\n            <p>" + options.body + "</p>\n          </div>\n        ";
+          break;
+      }
     }
   }]);
 
--- src/jt-simple-modal.js
+++ src/jt-simple-modal.js
@@ -2,21 +2,156 @@
 
 class JT_Smodals {
 
-  constructor(args) {
-    this.options = Object.assign(this, {
+  constructor(options) {
+
+    this.options = {
       body: "",
-      type: "", 
-      header: "Attention!", 
+      type: "simple",
+      header: "Attention!",
       bodyIcon: "",
-      inputType: "text",
-      headerIcon: "",
+      headerIcon: "fa-info",
       customClass: '',
-      customContent: ""
-    }, args);
+      customContent: "",
+    };
+
+
+    this.setOptions(options);
+
+    this.eventHandler = (evt) => {
+      if (evt.target && (evt.target.id == this.options.htmlID || evt.target.id === 'jt-simple-modal-close')) {
+        this.closeModal();
+      }
+
+      if (evt.target && evt.target.id == 'jt-simple-modal-submit') {
+        if (typeof this.options.unlockOnClick === 'function') {
+          const val = document.getElementById('jt-simple-modal-input').value;
+          this.options.unlockOnClick(evt, val);
+        }
+        this.closeModal();
+      }
+
+      if (evt.target && evt.target.id == 'jt-simple-modal-confirm') {
+        if (typeof this.options.confirmOnClick === 'function') {
+          this.options.confirmOnClick(evt);
+        }
+        this.closeModal();
+      }
+    };
   }
 
-  logOptions() {
+  setOptions(options) {
+    const htmlID = 'jt-simple-modal-' + this.getNextId();
+    Object.assign(this.options, {htmlID}, options);
     console.log(this.options);
-  };
+  }
+
+  getOptions() {
+    return this.options;
+  }
+
+  getNextId() {
+    return document.querySelectorAll('.jt-simple-modal').length;
+  }
+
+  attachEvents() {
+    document.body.addEventListener('click', this.eventHandler);
+  }
+
+  removeEvents() {
+    document.body.removeEventListener('click', this.eventHandler);
+  }
+
+  openModal(options = {}) {
+    // Update the options if needed
+    this.setOptions(options);
+    // Append the modal to the body
+    document.body.insertAdjacentHTML('beforeend', this.prepareModal(options));
+    // Get and Set the modal element in var
+    this.modal = document.getElementById(this.options.htmlID);
+    // Change the modal style to flex to display the modal
+    this.modal.style.display = 'flex';
+    // Attach event handler
+    this.attachEvents();
+  }
+
+  closeModal() {
+    // Remove the moddal from the DOM
+    this.modal.parentNode.removeChild(this.modal);
+    // Reset the modal element in var
+    this.modal = null;
+    // Remove event handler if there are no modals
+    if (this.getNextId < 1) {
+      this.removeEvents();
+    }
+  }
+
+  prepareModal() {
+    const template = this.getTemplate(this.options);
+    return `
+      <div id='${this.options.htmlID}' class='jt-simple-modal ${this.options.customClass}'>
+        <div id='jt-simple-modal-container'>
+          <div id='jt-simple-modal-header'>
+            <i class='fa ${this.options.headerIcon}' aria-hidden='true'></i>
+            <h4>${this.options.header}</h4>
+            <i class='fa fa-times' id='jt-simple-modal-close' aria-hidden='true'></i>
+          </div>
+          <div id='jt-simple-modal-body'>
+            ${template}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getTemplate(options) {
+
+    if (!options || !options.type) {
+      return;
+    }
+
+    switch (options.type) {
+      case 'simple':
+        return `
+          <div class='jt-simple-modal-desc'>
+            <i class='fa ${options.bodyIcon}' aria-hidden='true'></i>
+            <p>${options.body}</p>
+          </div>
+        `;
+        break;
+
+      case 'unlock':
+        const unlockTemplate = `
+          <div class='jt-simple-modal-desc'>
+            <form id='jt-simple-modal-form'>
+              <input id="jt-simple-modal-input" placeholder="${this.options.unlockPlaceholder || 'test'}" name="unlockPassword" type="password">
+            </form>
+            <a href="#" class="btn btn-primary" id="jt-simple-modal-submit">Unlock</a>
+          </div>
+        `;
+        return unlockTemplate;
+        break;
+
+      case 'confirm':
+        return `
+          <div class='jt-simple-modal-desc'>
+            <i class='fa ${this.options.bodyIcon}' aria-hidden='true'></i>
+            <p>${this.options.body}</p>
+          </div>
+          <div class='jt-simple-modal-desc'>
+            <button href="#" id="jt-simple-modal-confirm">${this.options.confirmBtnText || 'OK'}</button>
+          </div>
+        `;
+        break;
+
+      default:
+        return `
+          <div class='jt-simple-modal-desc'>
+            <i class='fa ${options.bodyIcon}' aria-hidden='true'></i>
+            <p>${options.body}</p>
+          </div>
+        `
+        break;
+    }
+  }
 
 }
